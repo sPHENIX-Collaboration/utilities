139e95fce02f8f6be594848a52846879444458db
Jin Huang <blackcathj@gmail.com>
2015-07-28 10:39:00 -0400

    Update doxygen_mainpage.h
    Update to use PHG4Hitv1 instead of PHG4Hitv6

--- utils/rebuild/Doxygen_Assist/doxygen_mainpage.h
+++ utils/rebuild/Doxygen_Assist/doxygen_mainpage.h
@@ -17,9 +17,9 @@ Many feature of the website will help you navigate through the ocean of PHENIX s
 \li Ever want to find a class or function? It is easy now using the <b>"Search" box</b> on top right of every page. Type few initial letters it will give hints to the rest.
 \li Left side is a <b>navigation bar</b> that expands automatically with your browsing by default. You can also make these two independent of each other by toggling the arrows on left top.
 \li Want to browse the software <b>directory tree</b>? Check out the <a href="files.html"><i>Files</i> button in the navigation bar</a>.
-\li How a class is inherited? Check out the <b>Inheritance diagram</b> in the class pages. They are auto folded by default. Example: click <i>"Inheritance diagram for PHG4Hitv6"</i> in page \ref PHG4Hitv6. 
-\li Find out <b>inter-dependencies between files</b>. Example is on \ref PHG4Hitv6.cc page, you can try clicking to unfold <i>"Include dependency graph"</i> and <i>"files directly or indirectly include this file"</i>.
-\li Highlighted <b>source code</b> with crossreferencing. Example click <i>"Go to the source code"</i> in \ref PHG4Hitv6.cc .
+\li How a class is inherited? Check out the <b>Inheritance diagram</b> in the class pages. They are auto folded by default. Example: click <i>"Inheritance diagram for PHG4Hitv1"</i> in page \ref PHG4Hitv1. 
+\li Find out <b>inter-dependencies between files</b>. Example is on \ref PHG4Hitv1.cc page, you can try clicking to unfold <i>"Include dependency graph"</i> and <i>"files directly or indirectly include this file"</i>.
+\li Highlighted <b>source code</b> with crossreferencing. Example click <i>"Go to the source code"</i> in \ref PHG4Hitv1.cc .
 There are many more features, which you are welcome to explore.
 
 \subsection Crossreference Crossreferencing with sPHENIX Git Respository
