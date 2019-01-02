4c4cc5c85a0b97f513d89cc0f2fd0298c5ade24d
Jin Huang <jhuang@bnl.gov>
2019-01-02 14:38:05 -0500

    return check

--- jenkins/built-test/test-default.sh
+++ jenkins/built-test/test-default.sh
@@ -7,12 +7,16 @@
 # setenv LD_LIBRARY_PATH 	$WORKSPACE/install/lib:${LD_LIBRARY_PATH}
 # setenv CALIBRATIONROOT  $WORKSPACE/calibrations/
 
-# setenv OFFLINE_MAIN $WORKSPACE/install
-# setenv ONLINE_MAIN $WORKSPACE/install
-# setenv CALIBRATIONROOT  $WORKSPACE/calibrations
+setenv OFFLINE_MAIN $WORKSPACE/install
+setenv ONLINE_MAIN $WORKSPACE/install
+setenv CALIBRATIONROOT  $WORKSPACE/calibrations
 
 source /opt/sphenix/core/bin/sphenix_setup.csh;
 
+
+echo "======================================================="
+echo "env check";
+echo "======================================================="
 
 env;
 
@@ -27,9 +31,23 @@ echo "======================================================="
 
 /usr/bin/time -v root -b -q root -b -q 'Fun4All_G4_sPHENIX.C(20)' | & tee -a Fun4All_G4_sPHENIX.log;
 
+set build_ret = $?;
+
 ls -lhcrt
 
+echo "Build step - build - return $build_ret";
+
+ls -lhcrt
+
+if ($build_ret != 0) then
+	echo "======================================================="
+	echo "Failed build with return = ${build_ret}. ";
+	echo "======================================================="
+	exit $build_ret;
+endif
+
 echo "Build step - test - done";
+
 
 
 
