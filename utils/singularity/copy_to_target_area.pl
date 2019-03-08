a903e3da9cac28965fc8bdd90b805c4743682531
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-03-08 15:57:08 -0500

    copy build of any offline version, copy also index.html

--- utils/singularity/copy_to_target_area.pl
+++ utils/singularity/copy_to_target_area.pl
@@ -4,6 +4,7 @@ use strict;
 use warnings;
 use Getopt::Long;
 use File::Copy;
+use File::Path;
 use Cwd;
 use Env;
 use File::Basename;
@@ -12,19 +13,41 @@ my $opt_all = 0;
 my $opt_singularity = 0;
 my $opt_optsphenix = 0;
 my $opt_offline = 0;
-GetOptions('all' => \$opt_all, 'singularity' => \$opt_singularity, 'opt' => \$opt_optsphenix, 'offline' => \$opt_offline);
+my $opt_help = 0;
+my $opt_test = 0;
+my $opt_version = 'new';
+GetOptions('all' => \$opt_all, 'help' => \$opt_help, 'offline' => \$opt_offline, 'opt' => \$opt_optsphenix, 'singularity' => \$opt_singularity, 'test' => \$opt_test, 'version:s' => \$opt_version);
 
-if ($#ARGV < 0)
+if ($#ARGV < 0 || $opt_help>0)
 {
     print "usage: copy_to_target_area.pl <target dir>\n";
     print "options:\n";
     print "--all          : copy container, opt area and offline_main tar balls\n";
-    print "--opt          : create and copy opt area tar ball\n";
+    print "--help         : print this help\n";
     print "--offline      : create and copy offline_main tar ball\n";
+    print "--opt          : create and copy opt area tar ball\n";
     print "--singularity  : copy container\n";
+    print "--test         : dryrun, print commands but do not execute them\n";
     exit 1;
 }
-print "OFF: $OFFLINE_MAIN\n";
+if (!$opt_all && !$opt_singularity && !$opt_optsphenix && !$opt_offline)
+{
+    print "no tarball selected, select --all for all, --offline for offline, --opt for opt\n";
+    exit 1;
+}
+
+# get the version (new, root6, ...)
+my $version = basename($OFFLINE_MAIN);
+$version =~ s/\..{1,}//; # remove decimal point and version (1-n), even for ana build
+if ($version ne $opt_version)
+{
+    print "OFFLINE_MAIN version $version does not match requested version $opt_version\n";
+    print "source the sphenix_setup script like:\n";
+    print "source /opt/sphenix/core/bin/sphenix_setup.csh -n $opt_version\n";
+    print "and try again\n";
+    exit 1;
+}
+
 my $targetdir = $ARGV[0];
 my $sourcedir = sprintf("/cvmfs/sphenix.sdcc.bnl.gov");
 my $singularity_container = sprintf("%s/singularity/rhic_sl7_ext.simg",$sourcedir);
@@ -47,72 +70,124 @@ if (! -d $targetdir)
 if ($opt_singularity > 0 || $opt_all > 0)
 {
     print "copying singularity container\n";
-    copy($singularity_container, $targetdir);
+    if (! $opt_test)
+    {
+	copy($singularity_container, $targetdir);
+    }
 }
+
 my $curdir = getcwd();
 if ($opt_optsphenix > 0 || $opt_all > 0)
 {
+    my $opttargetdir = sprintf("%s/%s",$targetdir,$opt_version);
+    if (! $opt_test)
+    {
+	mkpath($opttargetdir);
+    }
     my $rootdir = sprintf("%s/root",$OFFLINE_MAIN);
     my $g4dir = sprintf("%s/geant4",$OFFLINE_MAIN);
     my $rootlink = readlink($rootdir);
     my $g4link = readlink($g4dir);
     my $tarcmd = sprintf("tar -cf %s %s",$opt_tmp_tarfile,$rootlink);
     print "tarcmd: $tarcmd\n";
-    system($tarcmd);
+    if (! $opt_test)
+    {
+	system($tarcmd);
+    }
     my $rootsoftl = sprintf("%s/root",dirname($rootlink));
     $tarcmd = sprintf("tar  -rf %s %s",$opt_tmp_tarfile,$rootsoftl);
     print "tarcmd: $tarcmd\n";
-    system($tarcmd);
+    if (! $opt_test)
+    {
+	system($tarcmd)
+    };
     $tarcmd = sprintf("tar  -rf %s %s",$opt_tmp_tarfile,$g4link);
     print "tarcmd: $tarcmd\n";
-    system($tarcmd);
+    if (! $opt_test)
+    {
+	system($tarcmd);
+    }
     my $g4softl = sprintf("%s/geant4",dirname($g4link));
     $tarcmd = sprintf("tar  -rf %s %s",$opt_tmp_tarfile,$g4softl);
     print "tarcmd: $tarcmd\n";
-    system($tarcmd);
+    if (! $opt_test)
+    {
+	system($tarcmd);
+    }
     foreach my $dir (@opt_dir_list)
     {
 	$tarcmd = sprintf("tar  -rf %s %s",$opt_tmp_tarfile,$dir);
         print "tarcmd: $tarcmd\n";
-	system($tarcmd);
+	if (! $opt_test)
+	{
+	    system($tarcmd);
+	}
     }
     my $zipcmd = sprintf("bzip2 %s",$opt_tmp_tarfile);
-    system($zipcmd);
+    if (! $opt_test)
+    {
+	system($zipcmd);
+    }
     my $zipfile = sprintf("%s.bz2",$opt_tmp_tarfile);
-    print "moving $zipfile to $targetdir\n";
+    print "moving $zipfile to $opttargetdir\n";
     if (-f $zipfile)
     {
-	move($zipfile, $targetdir);
+	move($zipfile, $opttargetdir);
     }
     else
     {
-	print "could not find $zipfile\n";
-	exit 1;
+	if (! $opt_test)
+	{
+	    print "could not find $zipfile\n";
+	    exit 1;
+	}
     }
 }
 
 if ($opt_offline > 0 || $opt_all > 0)
 {
-    my $offline_symlink = sprintf("/cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/release/release_new/new");
+    my $offtargetdir = sprintf("%s/%s",$targetdir,$opt_version);
+    if (! $opt_test)
+    {
+      mkpath($offtargetdir);
+    }
+    my $offline_symlink = sprintf("/cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/release/release_%s/%s",$opt_version,$opt_version);
     my $tarcmd = sprintf("tar -cf %s %s",$offline_tmp_tarfile,$offline_symlink);
     print "executing $tarcmd\n";
-    system($tarcmd);
+    if (! $opt_test)
+    {
+	system($tarcmd);
+    }
     $tarcmd = sprintf("tar -caf %s %s",$offline_tmp_tarfile,$OFFLINE_MAIN);
     print "executing $tarcmd\n";
-    system($tarcmd);
+    if (! $opt_test)
+    {
+	system($tarcmd);
+    }
     my $zipcmd = sprintf("bzip2 %s",$offline_tmp_tarfile);
-    system($zipcmd);
+    if (! $opt_test)
+    {
+	system($zipcmd);
+    }
     my $zipfile = sprintf("%s.bz2",$offline_tmp_tarfile);
-    print "moving $zipfile to $targetdir\n";
+    print "moving $zipfile to $offtargetdir\n";
     if (-f $zipfile)
     {
-	move($zipfile, $targetdir);
+	move($zipfile, $offtargetdir);
     }
     else
     {
-	print "could not find $zipfile\n";
-	exit 1;
+	if (! $opt_test)
+	{
+	    print "could not find $zipfile\n";
+	    exit 1;
+	}
     }
 }
 
-
+# finally copy the html page over
+print "copying index.html to $targetdir\n";
+if (!$opt_test)
+{
+  copy("index.html",$targetdir);
+}
