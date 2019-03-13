060c85a7fcc7f799e0bb04e978362c8c7d4084a5
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-03-13 09:44:18 -0400

    check if git repo exist to avoid weird errors when users build their own

--- utils/rebuild/build.pl
+++ utils/rebuild/build.pl
@@ -331,6 +331,23 @@ else
 {
     make_path($sourceDir, {mode => 0775}) unless -e $sourceDir;
     chdir $sourceDir;
+    my $statret = 0;
+    $ENV{'GIT_ASKPASS'} = 'true';
+    foreach my $repo (@gitrepos)
+    {
+	$gitcommand = sprintf("git ls-remote https://github.com/%s/%s.git > /dev/null 2>&1",$opt_repoowner, $repo);
+        my $iret = system($gitcommand);
+	if ($iret)
+	{
+	    print LOG "repository https://github.com/$opt_repoowner/$repo.git does not exist\n";
+	}
+        $statret += $iret;
+    }
+    if ($statret)
+    {
+        close(LOG);
+	exit 1;
+    }
     foreach my $repo (@gitrepos)
     {
 	$gitcommand = sprintf("git clone -q https://github.com/%s/%s.git",$opt_repoowner, $repo);
@@ -440,6 +457,8 @@ if ($opt_insure)
       }
    $insureCompileFlags = ' CC="insure gcc -g" CXX="insure g++" CCLD="insure g++"';
   }
+
+# switch OFFLINE_MAIN to new install area and create it
 $oldOfflineMain = $OFFLINE_MAIN;
 $OFFLINE_MAIN = $installDir;
 $ENV{OFFLINE_MAIN} = $installDir;
