8eb514fc21cad155c487afb09a33536994dda1dc
Chris Pinkenburg <pinkenburg@bnl.gov>
2019-10-07 12:22:49 -0400

    deal with G4 includes include G4String.hh vs include <Geant4/G4String.hh> caused by geant4-config --cflags

--- utils/rebuild/analyze_includecheck_log.pl
+++ utils/rebuild/analyze_includecheck_log.pl
@@ -127,15 +127,63 @@ close(F);
 
 foreach my $file (sort keys %FileWithAddIssue)
 {
-    print "add for $file:\n";
-    print "@{$FileWithAddIssue{$file}}\n";
+    my @addlist;
+# check if adding and removing is the same G4 include
+    my %remtmp;
     if (exists $FileWithRemoveIssue{$file})
     {
-	print "remove:\n";
-	print "@{$FileWithRemoveIssue{$file}}\n";
-        delete $FileWithRemoveIssue{$file};
+	foreach my $rems (sort @{$FileWithRemoveIssue{$file}})
+	{
+	    $remtmp{$rems} = $file;
+	}
     }
-    
+    foreach my $include (sort @{$FileWithAddIssue{$file}})
+    {
+#	chomp $include;
+	my @sp1 = split(/\"/,$include);
+#	print "$sp1[1]\n";
+if (defined($sp1[1]))
+ {
+	my $foundrem = 0;
+	foreach my $remchk (keys  %remtmp)
+	{
+#	    print "$remchk\n";
+	    if ($remchk =~ /$sp1[1]/)
+	    {
+#		print "found $sp1[1]\n";
+		delete $remtmp{$remchk};
+		$foundrem = 1;
+		last;
+	    }
+	}
+	if ($foundrem == 1)
+	{
+	    next;
+	}
+}
+	push(@addlist,$include);
+    }
+    if (exists $FileWithRemoveIssue{$file})
+    {
+	delete $FileWithRemoveIssue{$file};
+	foreach my $reminc (keys %remtmp)
+	{
+	    push(@{$FileWithRemoveIssue{$file}},$reminc);
+	}
+    }
+    if ($#addlist >= 0)
+    {
+	print "add for $file:\n";
+	print "@addlist\n";
+#    next;
+#    print "@{$FileWithAddIssue{$file}}\n";
+	if (exists $FileWithRemoveIssue{$file})
+	{
+	    print "remove:\n";
+	    print "@{$FileWithRemoveIssue{$file}}\n";
+	    delete $FileWithRemoveIssue{$file};
+	}
+    }
 }
 foreach my $file (sort keys %FileWithRemoveIssue)
 {
@@ -143,7 +191,7 @@ foreach my $file (sort keys %FileWithRemoveIssue)
     print "@{$FileWithRemoveIssue{$file}}\n";
 }
 
-sub checkadd
+sub checkadd # do not suggest to add these files
 {
     my $include_line = shift(@_);
     if (! &checkinclude($include_line))
@@ -163,8 +211,10 @@ sub checkadd
 	$include_line =~ /\"boost\/bimap\/detail\/bimap_core.hpp\"/ ||
 	$include_line =~ /\"boost\/bimap\/detail\/map_view_iterator.hpp\"/ ||
 	$include_line =~ /\"boost\/bimap\/relation\/structured_pair.hpp\"/ ||
+        $include_line =~ /boost\/container_hash\/extensions.hpp/ || 
 	$include_line =~ /\"boost\/core\/swap.hpp\"/ ||
         $include_line =~ /boost\/filesystem\/operations.hpp/ ||
+        $include_line =~ /boost\/filesystem\/path.hpp/ ||
 	$include_line =~ /\"boost\/format\/alt_sstream.hpp\"/ ||
 	$include_line =~ /\"boost\/format\/format_class.hpp\"/ ||
 	$include_line =~ /\"boost\/format\/format_fwd.hpp\"/ ||
@@ -175,14 +225,22 @@ sub checkadd
 	$include_line =~ /\"boost\/graph\/detail\/edge.hpp\"/ ||
 	$include_line =~ /\"boost\/graph\/graph_selectors.hpp\"/ ||
         $include_line =~ /\"boost\/iterator\/iterator_facade.hpp\"/ ||
+        $include_line =~ /\"boost\/lexical_cast\/bad_lexical_cast.hpp\"/ ||
 	$include_line =~ /\"boost\/move\/utility_core.hpp\" / ||
 	$include_line =~ /\"boost\/multi_index\/detail\/bidir_node_iterator.hpp\"/ ||
+	$include_line =~ /\"boost\/numeric\/interval\/interval.hpp\"/ ||
+	$include_line =~ /\"boost\/numeric\/interval\/policies.hpp\"/ ||
+	$include_line =~ /\"boost\/numeric\/interval\/utility.hpp\"/ ||
 	$include_line =~ /\"boost\/optional\/optional.hpp\"/ ||
 	$include_line =~ /\"boost\/pending\/property.hpp\"/ ||
 	$include_line =~ /\"boost\/property_tree\/detail\/exception_implementation.hpp\"/ ||
 	$include_line =~ /\"boost\/property_tree\/detail\/ptree_implementation.hpp\"/ ||
 	$include_line =~ /\"boost\/property_tree\/detail\/rapidxml.hpp\"/ ||
 	$include_line =~ /\"boost\/property_tree\/ptree_fwd.hpp\"/ ||
+ 	$include_line =~ /\"boost\/smart_ptr\/detail\/operator_bool.hpp"/ ||
+	$include_line =~ /\"boost\/smart_ptr\/make_shared_object.hpp\"/ ||
+ 	$include_line =~ /\"boost\/smart_ptr\/shared_ptr.hpp\" / ||
+	$include_line =~ /\"boost\/stacktrace\/stacktrace.hpp\"/ ||
         $include_line =~ /\"boost\/token_functions.hpp\"/ ||
 	$include_line =~ /\"boost\/tuple\/detail\/tuple_basic.hpp\"/ ||
 	$include_line =~ /\"boost\/type_index\/type_index_facade.hpp\"/ ||
@@ -193,6 +251,7 @@ sub checkadd
         $include_line =~ /Geant4\/G4ReferenceCountedHandle.hh/ ||
         $include_line =~ /<gsl\/gsl_const_cgs.h>/ ||
         $include_line =~ /<gsl\/gsl_vector_double.h>/ ||
+        $include_line =~ /mpfr.h/ ||
         $include_line =~ /\.icc/ ||
         $include_line =~ /\"Rtypes.h\"/ ||
         $include_line =~ /<Rtypes.h>/ ||
@@ -209,7 +268,7 @@ sub checkadd
     return 1;
 }
 
-sub checkremove
+sub checkremove # do not suggest to remove these files
 {
     my $include_line = shift(@_);
     if (! &checkinclude($include_line))
@@ -219,10 +278,15 @@ sub checkremove
     if (
 	$include_line =~ /<boost\/accumulators\/accumulators.hpp>/ ||
 	$include_line =~ /<boost\/accumulators\/statistics.hpp>/ ||
+	$include_line =~ /<boost\/algorithm\/string\/case_conv.hpp>/ ||
 	$include_line =~ /<boost\/bimap.hpp>/ ||
 	$include_line =~ /<boost\/bind.hpp>/ ||
         $include_line =~ /<boost\/filesystem.hpp>/ ||
         $include_line =~ /<boost\/format.hpp>/ ||
+        $include_line =~ /<boost\/functional\/hash.hpp>/ ||
+        $include_line =~ /<boost\/numeric\/interval.hpp>/ ||
+        $include_line =~ /<boost\/smart_ptr.hpp>/ ||
+        $include_line =~ /<boost\/stacktrace.hpp>/ ||
         $include_line =~ /<boost\/tuple\/tuple.hpp>/ ||
 	$include_line =~ /<Eigen\/Dense>/ ||
         $include_line =~ /<Eigen\/LU>/ ||
