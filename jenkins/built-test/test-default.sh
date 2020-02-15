caa5fe308b73a2035192740b41dbd466d8e3be6a
Jin Huang <jhuang@bnl.gov>
2020-02-15 18:48:51 -0500

    use sysname

--- jenkins/built-test/test-default.sh
+++ jenkins/built-test/test-default.sh
@@ -1,28 +1,25 @@
-#! tcsh -f
+#!/usr/bin/env bash -v
 
-if ($#argv != 4) then
-	
-	echo "Usage $0 root5 Fun4All_G4_sPHENIX number_event run_valgrind"
+if [ "$#" -ne 3 ]; then	
+	echo "Usage $0  Fun4All_G4_sPHENIX number_event run_valgrind"
 	exit 1;
-	
-endif
+fi
+if [ -z "${sysname}" ]; then
+	echo "Fatal error: Miss env sysname"
+	exit 1;
+fi
+if [ -z "$build_type" ]; then	
+	echo "Fatal error: Miss env build_type"
+	exit 1;
+fi
 
-set build_type = $1;
-set macro_name = $2;
-set number_event = $3;
-set run_valgrind = $4;
+macro_name=$1;
+number_event=$2;
+run_valgrind=$3;
 
+export OFFLINE_MAIN=$WORKSPACE/install
 
-# source /opt/sphenix/core/bin/sphenix_setup.csh -n; 
-
-# setenv workRootPath `pwd`;
-# setenv PATH 		$WORKSPACE/install/bin:${PATH}
-# setenv LD_LIBRARY_PATH 	$WORKSPACE/install/lib:${LD_LIBRARY_PATH}
-# setenv CALIBRATIONROOT  $WORKSPACE/calibrations/
-
-setenv OFFLINE_MAIN $WORKSPACE/install
-echo "source /opt/sphenix/core/bin/sphenix_setup.csh $build_type;"
-source /opt/sphenix/core/bin/sphenix_setup.csh $build_type;
+source /cvmfs/sphenix.sdcc.bnl.gov/x8664_sl7/opt/sphenix/core/bin/sphenix_setup.sh $build_type;
 
 echo "======================================================="
 echo "env check";
@@ -36,25 +33,25 @@ pwd;
 ls -lhc
 
 
-set valgrind_prefix = "";
+valgrind_prefix="";
 
-if ($run_valgrind > 0) then
+if [$run_valgrind > 0]; then
 	
-	set valgrind_sup = '';
+	valgrind_sup='';
 
-	if (-e $ROOTSYS/root.supp) then
-	    set valgrind_sup = "--suppressions=$ROOTSYS/root.supp";
-	endif	
+	if [-f $ROOTSYS/root.supp]; then
+	    valgrind_sup="--suppressions=$ROOTSYS/root.supp";
+	fi	
 	
 	# set valgrind_prefix = "valgrind -v  --num-callers=30 --leak-check=full --error-limit=no --log-file=${macro_name}.valgrind $valgrind_sup --xml=yes --xml-file=${macro_name}.valgrind.xml --leak-resolution=high"
 	# set valgrind_prefix = "valgrind -v  --num-callers=30 --leak-check=full --error-limit=no --log-file=${macro_name}.valgrind $valgrind_sup  --leak-resolution=high"
 	# set valgrind_prefix = "valgrind -v --gen-suppressions=all  --num-callers=30 --leak-check=full --error-limit=no --log-file=${macro_name}.valgrind $valgrind_sup  --leak-resolution=high"
-	set valgrind_prefix = "valgrind -v --num-callers=30 --gen-suppressions=all --leak-check=full --error-limit=no --log-file=${macro_name}.valgrind $valgrind_sup --xml=yes --xml-file=${macro_name}.valgrind.xml --leak-resolution=high"
+	valgrind_prefix="valgrind -v --num-callers=30 --gen-suppressions=all --leak-check=full --error-limit=no --log-file=${macro_name}.valgrind $valgrind_sup --xml=yes --xml-file=${macro_name}.valgrind.xml --leak-resolution=high"
 	
 	which valgrind
 	echo "valgrind_prefix = ${valgrind_prefix}"
 	
-endif
+fi
 
 echo "======================================================="
 echo "Start test";
@@ -63,23 +60,23 @@ echo "======================================================="
 # Test case to produce memory error
 # echo '{int ret = gSystem->Load("libg4bbc.so"); cout <<"Load libg4detectors = "<<ret<<endl;assert(ret == 0);BbcVertexFastSimReco* bbcvertex = new BbcVertexFastSimReco();BbcVertexFastSimReco* bbcvertex = new BbcVertexFastSimReco();int a[2] = {0}; a[3] = 1;exit(0);}' > test.C
 
-/usr/bin/time -v ${valgrind_prefix} root.exe -b -q "${macro_name}.C(${number_event})" | & tee -a ${macro_name}.log;
+/usr/bin/time -v ${valgrind_prefix} root.exe -b -q "${macro_name}.C(${number_event})" 2>&1 | tee -a ${macro_name}.log;
 
-set build_ret = $?;
+build_ret=$?;
 
 ls -lhcrt
 
 echo "Build step - build - return $build_ret";
 
-if ($build_ret != 0) then
+if [$build_ret -ne 0]; then
 	echo "======================================================="
 	echo "Failed run with return = ${build_ret}. ";
 	echo "======================================================="
 	
 	# if ($run_valgrind == 0) then
-		exit $build_ret;
+	exit $build_ret;
 	# endif
-endif
+fi
 
 
 
