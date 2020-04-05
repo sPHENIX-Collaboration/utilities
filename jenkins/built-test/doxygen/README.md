fa0e1c986ecfc4d5a5846e6e45095f4fb6e0d2fa
Jin Huang <jhuang@bnl.gov>
2020-04-04 22:57:34 -0400

    move doxygen to jenkins build

---
+++ jenkins/built-test/doxygen/README.md
@@ -0,0 +1,9 @@
+[![Build Status](https://web.racf.bnl.gov/jenkins-sphenix/buildStatus/icon?job=EIC/doxygen)](https://web.racf.bnl.gov/jenkins-sphenix/job/EIC/job/doxygen/)
+[![Doxygen](https://img.shields.io/badge/code%20reference-Doxygen-green.svg)](https://eic.github.io/doxygen)
+
+Source file for [JekninCI doxygen documentation generator](https://eic.github.io/doxygen). 
+
+For mainance edits: 
+
+* Text, figure, header and footer are in [Doxygen_Assist/](./Doxygen_Assist)
+* Selection of git repositories and git behavior at [jenkins/EIC/Doxygen.Jenkins](/jenkins/EIC/Doxygen.Jenkins)
---
+++ jenkins/built-test/doxygen/doxygen.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+pwd;
+
+env;
+
+echo "-------- source setup ---------"
+source /opt/sphenix/core/bin/sphenix_setup.sh -n
+
+env;
+
+echo "doxygen at " `which doxygen`
+echo "dot at " `which dot`
+
+
+echo "-------- Doxyfile additional setup ---------"
+echo "CVS_STRIP_FROM_PATH = " `pwd` | tee -a Doxyfile
+echo "INPUT                  = " `/bin/ls -d */` | tee -a Doxyfile
+
+echo "-------- start doxygen ---------"
+
+doxygen Doxyfile >& doxygen.log
+build_ret=$?;
+
+echo "Build step - build - done. return = ${build_ret}";
+
+if (( $build_ret != 0 )); then
+	echo "======================================================="
+	echo "Failed doxygen build with return = ${build_ret}. Print end of log:";
+	echo "======================================================="
+    tail -n 100 *.log
+	exit $build_ret;
+fi
+
+echo "-------- publishing ---------"
+
+cd html/
+
+pwd
+ll
+
+git init
+git checkout -b gh-pages
+git status
+git add .
+git commit -am "doxygen build at `date`"
+
+git remote add origin git@github.com:eic/doxygen.git
+git push -f origin gh-pages
+
