62a3da710755417f77e4c06b0ab6a205c706a078
Jin Huang <jhuang@bnl.gov>
2020-12-31 15:27:52 -0500

    Update github-comment-label.py

--- jenkins/built-test/github-comment-label.py
+++ jenkins/built-test/github-comment-label.py
@@ -17,8 +17,8 @@ from github import Github, GithubIntegration
 
 # Parse pull requests e.g. https://github.com/sPHENIX-Test/coresoftware/pull/86
 if os.getenv("ghprbPullLink") is None:
-	print('Ignore this build as ghprbPullLink is emtpy')
-	exit(0)
+    print('Ignore this build as ghprbPullLink is emtpy')
+    exit(0)
 
 ghprbPullLink = os.environ['ghprbPullLink']
 
@@ -79,7 +79,7 @@ LabelStatus = os.environ['LabelStatus']
 
 LabelURL = os.getenv("BUILD_URL")
 if os.getenv("githubComment") is not None:
-	LabelURL = os.getenv("LabelURL")
+    LabelURL = os.getenv("LabelURL")
 
 #########################
 # Talk to GitHub
@@ -93,39 +93,39 @@ pr = repo.get_pull(pullRequestID)
 
 # githubComment
 if os.getenv("githubComment") is not None:
-	print ("Create comment -", os.getenv("githubComment"));
+    print ("Create comment -", os.getenv("githubComment"));
     ############## COMMENT OUT TO TMP DISABLE ##############
-	pr.create_issue_comment(os.getenv("githubComment"))
+    pr.create_issue_comment(os.getenv("githubComment"))
 
 # LabelStatus
 if LabelStatus in labelStatusOperations:
-	assert os.getenv("LabelCategory") is not None, 'LabelCategory is empty'
-	LabelCategory = os.getenv("LabelCategory")
-	
-	repo_labels = [label.name for label in repo.get_labels()];
-	print('Repository labels - ', repo_labels)
-	
-	labelList = pr.get_labels();
+    assert os.getenv("LabelCategory") is not None, 'LabelCategory is empty'
+    LabelCategory = os.getenv("LabelCategory")
     
-	labels = [label.name for label in labelList];
-	print('Current PR labels - ', labels)
-	
-	for status in labelStatusOperations:
-		labelName = "CI-{}-{}".format(LabelCategory, status)
-		
-		if not (labelName in repo_labels):
-			# create_label(name, color, description=NotSet)
-			print ('create label ', labelName, labelStatusColors[status], "{} {}".format(LabelCategory, labelStatusDesciptions[status]))
-			repo.create_label(labelName, labelStatusColors[status], "{} {}".format(LabelCategory, labelStatusDesciptions[status]));
-			
-		if LabelStatus == status:
-			if not (labelName in labels):
-				print ("add label ", labelName);
-    			############## COMMENT OUT TO TMP DISABLE ##############
-				pr.add_to_labels(labelName);
-		else:
-			if labelName in labels:
-				print ("remove label ", labelName);
-    			############## COMMENT OUT TO TMP DISABLE ##############
-				pr.remove_from_labels(labelName);
+    repo_labels = [label.name for label in repo.get_labels()];
+    print('Repository labels - ', repo_labels)
+    
+    labelList = pr.get_labels();
+    
+    labels = [label.name for label in labelList];
+    print('Current PR labels - ', labels)
+    
+    for status in labelStatusOperations:
+        labelName = "CI-{}-{}".format(LabelCategory, status)
+        
+        if not (labelName in repo_labels):
+            # create_label(name, color, description=NotSet)
+            print ('create label ', labelName, labelStatusColors[status], "{} {}".format(LabelCategory, labelStatusDesciptions[status]))
+            repo.create_label(labelName, labelStatusColors[status], "{} {}".format(LabelCategory, labelStatusDesciptions[status]));
+            
+        if LabelStatus == status:
+            if not (labelName in labels):
+                print ("add label ", labelName);
+                ############## COMMENT OUT TO TMP DISABLE ##############
+                pr.add_to_labels(labelName);
+        else:
+            if labelName in labels:
+                print ("remove label ", labelName);
+                ############## COMMENT OUT TO TMP DISABLE ##############
+                pr.remove_from_labels(labelName);
 
