a268fc2c5ef6b79bfa7099b5b28d766c20678998
Jin Huang <jhuang@bnl.gov>
2021-02-22 10:59:14 -0500

    Check if virtual env exits

--- jenkins/built-test/github-pygithub-setup.sh
+++ jenkins/built-test/github-pygithub-setup.sh
@@ -1,8 +1,18 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 env
 
 ls -lhv
+
+if [ -d "env/bin" ] 
+then
+    echo "Directory env exists, and skip virtual env initialization. Here is its content:"
+    ls -lhv env/
+    ls -lhv env/bin
+    exit
+else
+    echo "Continue to setup virtual env"
+fi
 
 ~/anaconda3/bin/virtualenv env
 source env/bin/activate
