f21df1f4e3e9643f1f47b8924191a803f85668fc
Jin Huang <jhuang@bnl.gov>
2021-12-21 20:46:33 -0500

    Add time out

--- jenkins/built-test/test-default-detector.sh
+++ jenkins/built-test/test-default-detector.sh
@@ -71,7 +71,7 @@ echo "======================================================="
 # Test case to produce memory error
 # echo '{int ret = gSystem->Load("libg4bbc.so"); cout <<"Load libg4detectors = "<<ret<<endl;assert(ret == 0);BbcVertexFastSimReco* bbcvertex = new BbcVertexFastSimReco();BbcVertexFastSimReco* bbcvertex = new BbcVertexFastSimReco();int a[2] = {0}; a[3] = 1;exit(0);}' > test.C
 
-( /usr/bin/time -v ${valgrind_prefix} root.exe -b -q "${macro_name}.C(${number_event})" ; echo $? > return.tmp ) 2>&1 | tee ${macro_name}.log;
+( /usr/bin/time -v  timeout --preserve-status --kill-after=1s --signal=9 1d  ${valgrind_prefix} root.exe -b -q "${macro_name}.C(${number_event})" ; echo $? > return.tmp ) 2>&1 | tee ${macro_name}.log;
 
 build_ret=`cat return.tmp`;
 
