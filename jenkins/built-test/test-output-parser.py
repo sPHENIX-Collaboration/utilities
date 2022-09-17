1e353c071d4c2d092295f171fc0f55b16a0fd5a7
Jin Huang <jhuang@bnl.gov>
2022-09-17 15:16:43 -0400

    Update test-output-parser.py

--- jenkins/built-test/test-output-parser.py
+++ jenkins/built-test/test-output-parser.py
@@ -11,9 +11,15 @@ input_files = ["Fun4All_G4_sPHENIX.log"]
 output_csv = "Fun4All_G4_sPHENIX.csv"
 
 search_items = {
-    'Time (s)': '\sUser time \(seconds\):\s([-+]?[0-9]*\.?[0-9]*)$',
-    'Memory (kB)': '\sMaximum resident set size \(kbytes\):\s([-+]?[0-9]*\.?[0-9]*)$',
+    'Time (s)': '\sUser time \(seconds\):\s*([-+]?[0-9]*\.?[0-9]*)$',
+    'Memory (kB)': '\sMaximum resident set size \(kbytes\):\s*([-+]?[0-9]*\.?[0-9]*)$',
     }
+
+search_dynamic_items = {
+    'Module accumulated time (ms)': '([A-Za-z0-9_]+): accumulated time \(ms\):\s*([-+]?[0-9]*\.?[0-9]*)',
+    'Module per event time (ms)': '([A-Za-z0-9_]+): per event time \(ms\):\s*([-+]?[0-9]*\.?[0-9]*)',
+    }
+
 
 parser = argparse.ArgumentParser(description='Parse log file into data CSV.')
 parser.add_argument('--input_files',type=str, nargs='+', 
@@ -30,21 +36,30 @@ print("Input arguments: ",args)
 #%% init 
 
 search_compile = {}
+search_dynamic_compile = {}
 
 for label, exp in search_items.items():
     print(f"Search for {label} with regular expression [{exp}]")
     search_compile[label] = re.compile(exp)
-
+for label, exp in search_dynamic_items.items():
+    print(f"Dynamic search for {label} with regular expression [{exp}]")
+    search_dynamic_compile[label] = re.compile(exp)
 
 search_results = {}
+search_dynamic_results = {}
+
 for label in search_compile.keys():
     search_results[label] = {} # dictionary of file -> value
 search_results['STDOUT Linecount'] = {}
+for label in search_dynamic_items.keys():
+    search_dynamic_results[label] = {} # dictionary of file -> value
 
 #%% processing
 
 
 for input_file in input_files:
+    print (f"Processing {input_file}")
+
     with open(input_file, 'r') as file1:
 
         Lines = file1.readlines()
@@ -63,12 +78,26 @@ for input_file in input_files:
                     print (f"{line.strip()} -> { match.group(1) }")
                     search_results[label][input_file] = float(match[1])
 
+            for label,reexp in search_dynamic_compile.items():
+
+                match = reexp.search(line)
+
+                if match:
+                    print (f"{line.strip()} -> { match.groups()}")
+
+                    if match[1] not in search_dynamic_results[label].keys():
+                        search_dynamic_results[label][match[1]] = []
+
+                    search_dynamic_results[label][match[1]].append( float(match[2]) )
+
         search_results['STDOUT Linecount'][input_file] = count
         file1.close()
 
 # %% results
 
 pprint.pprint(search_results);
+
+pprint.pprint(search_dynamic_results);
 
 #%% to csv
 
@@ -94,5 +123,29 @@ for label,result_dict in search_results.items():
         f.close()
     with open(csv_name, 'r') as f:
         print(f.read())
+
+
+for label,result_dict in search_dynamic_results.items():
+
+    label_filename = label.replace(" ", "_")
+    csv_name = f"{output_csv}_{label_filename}.csv"
+    print (f"Output results for {label} to {csv_name}")
+
+    module_list = []
+    value_list = []
+
+    with open(csv_name, 'w') as f:
+
+        for module_name,result_values in result_dict.items():
+            module_list.append(module_name)
+            value_list.append(str(sum(result_values) / len(result_values)))
+
+        f.write("{}\n".format(",\t".join(module_list)))
+        f.write("{}\n".format(",\t".join(value_list)))
+        f.close()
+    with open(csv_name, 'r') as f:
+        print(f.read())
+    print(f"--- {len(module_list)} Entries ---")
+    
 
 # %%
