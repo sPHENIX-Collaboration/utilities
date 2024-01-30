edc372b7fb7e1e5827916b5d478a65cc84ce629e
Jin Huang <jhuang@bnl.gov>
2024-01-29 22:27:54 -0500

    Update clang-tidy.sh

--- jenkins/built-test/clang-tidy.sh
+++ jenkins/built-test/clang-tidy.sh
@@ -1,14 +1,15 @@
-#!/bin/tcsh
+#!/bin/bash
 
-source /opt/sphenix/core/bin/sphenix_setup.csh -n; 
+source /opt/sphenix/core/bin/sphenix_setup.sh -n; 
 
 which clang-tidy; 
 
-if (-e  clang-tidy-result.txt) then
-	rsync -avl --remove-source-files clang-tidy-result.txt clang-tidy-result.txt.backup
-endif
+if test -f clang-tidy-result.txt; then
+  mv -fv clang-tidy-result.txt clang-tidy-result.txt.backup
+fi
 
-run-clang-tidy ./coresoftware -- -Wall -Werror -Wshadow -std=c++17 -I$OFFLINE_MAIN/include -isystem$ROOTSYS/include -I$G4_MAIN/include -I$G4_MAIN/include/Geant4 -I$OPT_SPHENIX/include -o clang-tidy-result.txt
+shopt -s globstar
+clang-tidy ./coresoftware/**/*.cc ./coresoftware/**/*.cpp -- -Wall -Werror -Wshadow -std=c++17 -I$OFFLINE_MAIN/include -isystem$ROOTSYS/include -I$G4_MAIN/include -I$G4_MAIN/include/Geant4 -I$OPT_SPHENIX/include > clang-tidy-result.txt
 
 ls -hvl $PWD/clang-tidy-result.txt
 wc -l clang-tidy-result.txt
