314fd44cd588a615e0eb6a68ef152d8f2f6cfa34
Jin Huang <jhuang@bnl.gov>
2024-01-29 22:06:03 -0500

    Update clang-tidy.sh

--- jenkins/built-test/clang-tidy.sh
+++ jenkins/built-test/clang-tidy.sh
@@ -4,12 +4,11 @@ source /opt/sphenix/core/bin/sphenix_setup.csh -n;
 
 which clang-tidy; 
 
-if (-e  clang-tidy-result.xml) then
-	rsync -avl --remove-source-files clang-tidy-result.xml clang-tidy-result.xml.backup
+if (-e  clang-tidy-result.txt) then
+	rsync -avl --remove-source-files clang-tidy-result.txt clang-tidy-result.txt.backup
 endif
 
-# cppcheck -q --inline-suppr  --enable=warning --enable=performance --platform=unix64 --inconclusive --xml --xml-version=2 -j 10 --std=c++20 ./coresoftware > & cppcheck-result.xml
-clang-tidy -p ./coresoftware -- -Wall -Werror -Wshadow -std=c++17 -I$OFFLINE_MAIN/include -isystem$ROOTSYS/include -I$G4_MAIN/include -I$G4_MAIN/include/Geant4 -I$OPT_SPHENIX/include -o clang-tidy-result.txt
+run-clang-tidy ./coresoftware -- -Wall -Werror -Wshadow -std=c++17 -I$OFFLINE_MAIN/include -isystem$ROOTSYS/include -I$G4_MAIN/include -I$G4_MAIN/include/Geant4 -I$OPT_SPHENIX/include -o clang-tidy-result.txt
 
 ls -hvl $PWD/clang-tidy-result.txt
 wc -l clang-tidy-result.txt
