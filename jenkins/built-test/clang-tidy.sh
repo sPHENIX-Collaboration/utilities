9961f55226cdaa71b57dc8ae69fc9d769f71096b
Jin Huang <jhuang@bnl.gov>
2024-02-15 12:34:39 -0500

    Update clang-tidy.sh

--- jenkins/built-test/clang-tidy.sh
+++ jenkins/built-test/clang-tidy.sh
@@ -14,7 +14,7 @@ cd ${WORKSPACE}/utilities/utils/rebuild/
 ./build.pl --stage 1 --to_stage=2 --source=${WORKSPACE} --workdir=${WORKSPACE}/build;
 
 cd ${WORKSPACE}
-ln -sbfv build/new/install.1 ./
+ln -sbfv build/new/install.1 ./install
 ls -lhcv
 
 echo "-----------------------------------"
