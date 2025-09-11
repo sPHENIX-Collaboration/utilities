228e95175b04eccf34e5e5b4d0077e925d9461a9
Joe Osborn <osbornjd91@gmail.com>
2025-09-11 15:26:17 -0400

    remove check for env variable

--- jenkins/built-test/test-tracking-reconstruction-prdf-QA-Gallery.sh
+++ jenkins/built-test/test-tracking-reconstruction-prdf-QA-Gallery.sh
@@ -12,7 +12,7 @@ cd $WORKSPACE/QA-gallery
 pwd
 
 cp -fv $WORKSPACE/macros/TrackingProduction/*prdf_reconstruction*.root* ./
-export qa_file_name_new=$WORKSPACE/macros/TrackingProduction/*prdf_reconstruction*.root
+export qa_file_name_new=$WORKSPACE/macros/TrackingProduction/prdf_reconstruction53877_qa.root
 ls -lhv
 
 echo "environment before "
@@ -23,17 +23,13 @@ echo "======================================================="
 echo "${name}: Reference";
 echo "======================================================="
 
-export qa_file_name_ref='None';
-echo "use reference = ${use_reference}"
-if [[ "${use_reference}" == "true" ]]; then
-	ln -svfb ../reference
+ln -svfb ../reference
 	
-	export qa_file_name_ref=`/bin/ls -1 reference/*.root`
+export qa_file_name_ref=`/bin/ls -1 reference/*.root`
 	
-	echo "Reference file: with $qa_file_name_ref"
-	ls -lhvc $qa_file_name_ref
+echo "Reference file: with $qa_file_name_ref"
+ls -lhvc $qa_file_name_ref
 	
-fi
 
 echo "======================================================="
 echo "${name}: Initiating environment";
