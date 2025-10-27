421c1c7ccbe1f31926ac1bb86d9341861d35b62d
Jin Huang <jhuang@bnl.gov>
2025-10-27 11:40:41 -0400

    try Chris' version

--- jenkins/built-test/clang-tidy-macros.sh
+++ jenkins/built-test/clang-tidy-macros.sh
@@ -28,7 +28,8 @@ if test -f clang-tidy-result.txt; then
 fi
 
 shopt -s globstar
-clang-tidy ./macros/**/*.cc ./macros/**/*.cpp ./macros/**/*.C -- -Wall -Werror -Wshadow -std=c++20 -Wno-dangling -isystem$WORKSPACE/macros/common  -isystem$OFFLINE_MAIN/include -isystem$ROOTSYS/include -isystem$G4_MAIN/include -isystem$G4_MAIN/include/Geant4  -isystem$OPT_SPHENIX/include -DHomogeneousField -DEVTGEN_HEPMC3 -DRAVE -DRaveDllExport= > clang-tidy-result.txt
+# clang-tidy ./macros/**/*.cc ./macros/**/*.cpp ./macros/**/*.C -- -Wall -Werror -Wshadow -std=c++20 -Wno-dangling -isystem$WORKSPACE/macros/common  -isystem$OFFLINE_MAIN/include -isystem$ROOTSYS/include -isystem$G4_MAIN/include -isystem$G4_MAIN/include/Geant4  -isystem$OPT_SPHENIX/include -DHomogeneousField -DEVTGEN_HEPMC3 -DRAVE -DRaveDllExport= > clang-tidy-result.txt
+clang-tidy ./macros/**/*.cc ./macros/**/*.cpp ./macros/**/*.C -- -Wall -Werror -Wshadow -std=c++20 -Wno-dangling -isystem$WORKSPACE/macros/common -isystem$OFFLINE_MAIN/include -isystem$ROOTSYS/include -isystem$G4_MAIN/include -isystem$G4_MAIN/include/Geant4 -isystem$OPT_SPHENIX/include -isystem$OFFLINE_MAIN/rootmacros -DHomogeneousField -DEVTGEN_HEPMC3 -DRAVE -DRaveDllExport=
 
 ls -hvl $PWD/clang-tidy-result.txt
 wc -l clang-tidy-result.txt
